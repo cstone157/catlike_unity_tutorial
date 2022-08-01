01b592c2972310467c839a01df5e382e01bebeba
Culver Stone <culver.stone57@gmail.com>
2022-07-31 21:27:52 -0700

    Added/started Lesson 03, using a function library for graphing different functions

---
+++ Assets/Scripts/Lesson03/FunctionLibrary.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using static UnityEngine.Mathf;
+
+public static class FunctionLibrary {
+    /**
+     * Create a wave for our points
+     */
+    public static float Wave (float x, float t) {
+		return Sin(PI * (x + t));
+	}
+
+    /**
+     * Create a multi wave for our points
+     */
+	public static float MultiWave (float x, float t) {
+//		float y = Sin(PI * (x + t));
+    float y = Sin(PI * (x + 0.5f * t));
+		y += 0.5f * Sin(2f * PI * (x + t));
+		return y * (2f / 3f);
+	}
+}
---
+++ Assets/Scripts/Lesson03/Graph2.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class Graph2 : MonoBehaviour {
+
+	[SerializeField]
+	Transform pointPrefab;
+
+    // Number of points to be created
+    [SerializeField, Range(10, 100)]
+	int resolution = 10;
+
+    // Which function should we render
+	[SerializeField, Range(0, 1)]
+	int function;
+
+    // The points that we are graphing  
+    Transform[] points;
+    void Awake () {
+        // Scale to stay within the 1 by 1 domain
+        float step = 2f / resolution;
+
+		var position = Vector3.zero;
+        var scale = Vector3.one * step;
+
+        // Create an array of points, for storing
+        points = new Transform[resolution];
+
+        for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i] = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+			point.localPosition = position;
+			point.localScale = scale;
+            point.SetParent(transform, false);
+		}
+    }
+
+    void Update () {
+        float time = Time.time;
+
+        // Loop trough our points and update their positions
+		for (int i = 0; i < points.Length; i++) {
+            Transform point = points[i];
+			Vector3 position = point.localPosition;
+//            position.y = FunctionLibrary.MultiWave(position.x, time);
+
+            if (function == 0) {
+				position.y = FunctionLibrary.Wave(position.x, time);
+			}
+			else {
+				position.y = FunctionLibrary.MultiWave(position.x, time);
+			}
+
+			point.localPosition = position;
+        }
+	}
+}
