8181f7c6df1af5513c21fac181c6070ef0101057
Culver Stone <culver.stone57@gmail.com>
2022-08-02 20:32:13 -0700

    Added the z-axis to the graph

--- Assets/Scripts/Lesson03/FunctionLibrary.cs
+++ Assets/Scripts/Lesson03/FunctionLibrary.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 using static UnityEngine.Mathf;
 
 public static class FunctionLibrary {
-	public delegate float Function (float x, float t);
+	public delegate float Function (float x, float z, float t);
+
 	public enum FunctionName { Wave, MultiWave, Ripple }
 	static Function[] functions = { Wave, MultiWave, Ripple };
-	
+
 	public static Function GetFunction (FunctionName name) {
 		return functions[(int)name];
 	}
@@ -13,25 +14,25 @@ public static class FunctionLibrary {
   /**
     * Create a wave for our points
     */
-  public static float Wave (float x, float t) {
-		return Sin(PI * (x + t));
+  public static float Wave (float x, float z, float t) {
+		return Sin(PI * (x + z + t));
 	}
 
   /**
     * Create a multi wave for our points
     */
-	public static float MultiWave (float x, float t) {
-//		float y = Sin(PI * (x + t));
-    float y = Sin(PI * (x + 0.5f * t));
-		y += 0.5f * Sin(2f * PI * (x + t));
-		return y * (2f / 3f);
+	public static float MultiWave (float x, float z, float t) {
+    	float y = Sin(PI * (x + 0.5f * t));
+		y += 0.5f * Sin(2f * PI * (z + t));
+		y += Sin(PI * (x + z + 0.25f * t));
+		return y * (1f / 2.5f);
 	}
 
   /**
     * Create a ripple for our points
     */
-  public static float Ripple (float x, float t) {
-		float d = Abs(x);
+  public static float Ripple (float x, float z, float t) {
+		float d = Sqrt(x * x + z * z);
 		float y = Sin(PI * (4f * d - t));
 		return y / (1f + 10f * d);
 	}
--- Assets/Scripts/Lesson03/Graph2.cs
+++ Assets/Scripts/Lesson03/Graph2.cs
@@ -24,11 +24,18 @@ public class Graph2 : MonoBehaviour {
         var scale = Vector3.one * step;
 
         // Create an array of points, for storing
-        points = new Transform[resolution];
+        points = new Transform[resolution * resolution];
 
-        for (int i = 0; i < points.Length; i++) {
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+            if (x == resolution) {
+				x = 0;
+                z += 1;
+			}
+
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
+            position.x = (x + 0.5f) * step - 1f;
+            position.z = (z + 0.5f) * step - 1f;
+            
 			point.localPosition = position;
 			point.localScale = scale;
             point.SetParent(transform, false);
@@ -43,7 +50,7 @@ public class Graph2 : MonoBehaviour {
 		for (int i = 0; i < points.Length; i++) {
             Transform point = points[i];
 			Vector3 position = point.localPosition;
-            position.y = f(position.x, time);
+            position.y = f(position.x, position.z, time);
 			point.localPosition = position;
         }
 	}
