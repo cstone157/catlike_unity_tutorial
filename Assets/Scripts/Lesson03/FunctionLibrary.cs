90d04ee4f7dc031b95c5b969aa62e0347bed28de
Culver Stone <culver.stone57@gmail.com>
2022-08-07 21:42:15 -0700

    Added sphere and torus

--- Assets/Scripts/Lesson03/FunctionLibrary.cs
+++ Assets/Scripts/Lesson03/FunctionLibrary.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 using static UnityEngine.Mathf;
 
 public static class FunctionLibrary {
-	public delegate float Function (float x, float z, float t);
+//	public delegate float Function (float x, float z, float t);
+	public delegate Vector3 Function (float u, float v, float t);
 
-	public enum FunctionName { Wave, MultiWave, Ripple }
-	static Function[] functions = { Wave, MultiWave, Ripple };
+	public enum FunctionName { Wave, MultiWave, Ripple, Sphere, Torus }
+	static Function[] functions = { Wave, MultiWave, Ripple, Sphere, Torus };
 
 	public static Function GetFunction (FunctionName name) {
 		return functions[(int)name];
@@ -14,26 +15,143 @@ public static class FunctionLibrary {
   /**
     * Create a wave for our points
     */
-  public static float Wave (float x, float z, float t) {
-		return Sin(PI * (x + z + t));
+	public static Vector3 Wave (float u, float v, float t) {
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (u + v + t));
+		p.z = v;
+		return p;
 	}
 
   /**
     * Create a multi wave for our points
     */
-	public static float MultiWave (float x, float z, float t) {
-    	float y = Sin(PI * (x + 0.5f * t));
-		y += 0.5f * Sin(2f * PI * (z + t));
-		y += Sin(PI * (x + z + 0.25f * t));
-		return y * (1f / 2.5f);
+	public static Vector3 MultiWave (float u, float v, float t) {
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (u + 0.5f * t));
+		p.y += 0.5f * Sin(2f * PI * (v + t));
+		p.y += Sin(PI * (u + v + 0.25f * t));
+		p.y *= 1f / 2.5f;
+		p.z = v;
+		return p;
 	}
 
   /**
     * Create a ripple for our points
     */
-  public static float Ripple (float x, float z, float t) {
-		float d = Sqrt(x * x + z * z);
-		float y = Sin(PI * (4f * d - t));
-		return y / (1f + 10f * d);
+	public static Vector3 Ripple (float u, float v, float t) {
+		float d = Sqrt(u * u + v * v);
+		Vector3 p;
+		p.x = u;
+		p.y = Sin(PI * (4f * d - t));
+		p.y /= 1f + 10f * d;
+		p.z = v;
+		return p;
+	}
+
+   /**
+    * Create a sphere of our points
+    */
+	public static Vector3 Sphere (float u, float v, float t) {
+		Vector3 p;
+		/*
+		// Circle
+		p.x = 0f;
+		p.y = 0f;
+		p.z = 0f;
+
+		// Cylnder
+		p.x = Sin(PI * u);
+		p.y = v;
+		p.z = Cos(PI * u);
+
+		// Sphere with collapsing radius
+		float r = Cos(0.5f * PI * v);
+		Vector3 p;
+		p.x = r * Sin(PI * u);
+		p.y = v;
+		p.z = r * Cos(PI * u);
+
+		// A Sphere
+		float r = Cos(0.5f * PI * v);
+		Vector3 p;
+		p.x = r * Sin(PI * u);
+		p.y = Sin(PI * 0.5f * v);
+		p.z = r * Cos(PI * u);
+
+		// A scaling sphere (shrinking)
+		float r = 0.5f + 0.5f * Sin(PI * t);
+		float s = r * Cos(0.5f * PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(0.5f * PI * v);
+		p.z = s * Cos(PI * u);
+
+		// Sphere with vertical bands
+		float r = 0.9f + 0.1f * Sin(8f * PI * u);
+		float s = r * Cos(0.5f * PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(0.5f * PI * v);
+		p.z = s * Cos(PI * u);
+
+		// Shpere with horizontal bands
+		float r = 0.9f + 0.1f * Sin(8f * PI * v);
+		float s = r * Cos(0.5f * PI * v);
+		Vector3 p;
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(0.5f * PI * v);
+		p.z = s * Cos(PI * u);
+
+		*/
+
+		// Spiral Sphere
+		float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+		float s = r * Cos(0.5f * PI * v);
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(0.5f * PI * v);
+		p.z = s * Cos(PI * u);
+		return p;
+	}
+
+   /**
+    * Create a torus of our points
+    */
+	public static Vector3 Torus (float u, float v, float t) {
+		Vector3 p;
+		/*
+		// Pulled apart shpere
+		float r = 1f;
+		float s = 0.5f + r * Cos(0.5f * PI * v);
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(0.5f * PI * v);
+		p.z = s * Cos(PI * u);
+
+		// A self-intersecting spindle torus.
+		float r = 1f;
+		float s = 0.5f + r * Cos(PI * v);
+		p.x = s * Sin(PI * u);
+		p.y = r * Sin(PI * v);
+		p.z = s * Cos(PI * u);
+
+		// A ring torus.
+		float r1 = 0.75f;
+		float r2 = 0.25f;
+		float s = r1 + r2 * Cos(PI * v);
+		p.x = s * Sin(PI * u);
+		p.y = r2 * Sin(PI * v);
+		p.z = s * Cos(PI * u);
+		*/
+
+		// Twisting torus.
+		float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+		float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+		float s = r1 + r2 * Cos(PI * v);
+		p.x = s * Sin(PI * u);
+		p.y = r2 * Sin(PI * v);
+		p.z = s * Cos(PI * u);
+
+		return p;
 	}
 }
--- Assets/Scripts/Lesson03/Graph2.cs
+++ Assets/Scripts/Lesson03/Graph2.cs
@@ -17,41 +17,31 @@ public class Graph2 : MonoBehaviour {
     // The points that we are graphing  
     Transform[] points;
     void Awake () {
-        // Scale to stay within the 1 by 1 domain
-        float step = 2f / resolution;
+		float step = 2f / resolution;
+		var scale = Vector3.one * step;
 
-		var position = Vector3.zero;
-        var scale = Vector3.one * step;
-
-        // Create an array of points, for storing
-        points = new Transform[resolution * resolution];
-
-        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
-            if (x == resolution) {
-				x = 0;
-                z += 1;
-			}
-
-            Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = (x + 0.5f) * step - 1f;
-            position.z = (z + 0.5f) * step - 1f;
-            
-			point.localPosition = position;
+		points = new Transform[resolution * resolution];
+		for (int i = 0; i < points.Length; i++) {
+			Transform point = points[i] = Instantiate(pointPrefab);
 			point.localScale = scale;
-            point.SetParent(transform, false);
+			point.SetParent(transform, false);
 		}
-    }
+	}
 
     void Update () {
-        float time = Time.time;
-        FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
-
-        // Loop trough our points and update their positions
-		for (int i = 0; i < points.Length; i++) {
-            Transform point = points[i];
-			Vector3 position = point.localPosition;
-            position.y = f(position.x, position.z, time);
-			point.localPosition = position;
-        }
+		FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+		float time = Time.time;
+        float step = 2f / resolution;
+        float v = 0.5f * step - 1f;
+		for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+			if (x == resolution) {
+				x = 0;
+				z += 1;
+				v = (z + 0.5f) * step - 1f;
+			}
+			float u = (x + 0.5f) * step - 1f;
+			//float v = (z + 0.5f) * step - 1f;
+			points[i].localPosition = f(u, v, time);
+		}
 	}
 }
