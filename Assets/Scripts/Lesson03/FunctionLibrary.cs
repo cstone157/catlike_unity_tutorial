52dc65efe726ecb0db71d74ea7bdcbc4d866aa6c
Culver Stone <culver.stone57@gmail.com>
2022-08-07 21:43:10 -0700

    Cleaned up my comments

--- Assets/Scripts/Lesson03/FunctionLibrary.cs
+++ Assets/Scripts/Lesson03/FunctionLibrary.cs
@@ -55,56 +55,6 @@ public static class FunctionLibrary {
     */
 	public static Vector3 Sphere (float u, float v, float t) {
 		Vector3 p;
-		/*
-		// Circle
-		p.x = 0f;
-		p.y = 0f;
-		p.z = 0f;
-
-		// Cylnder
-		p.x = Sin(PI * u);
-		p.y = v;
-		p.z = Cos(PI * u);
-
-		// Sphere with collapsing radius
-		float r = Cos(0.5f * PI * v);
-		Vector3 p;
-		p.x = r * Sin(PI * u);
-		p.y = v;
-		p.z = r * Cos(PI * u);
-
-		// A Sphere
-		float r = Cos(0.5f * PI * v);
-		Vector3 p;
-		p.x = r * Sin(PI * u);
-		p.y = Sin(PI * 0.5f * v);
-		p.z = r * Cos(PI * u);
-
-		// A scaling sphere (shrinking)
-		float r = 0.5f + 0.5f * Sin(PI * t);
-		float s = r * Cos(0.5f * PI * v);
-		Vector3 p;
-		p.x = s * Sin(PI * u);
-		p.y = r * Sin(0.5f * PI * v);
-		p.z = s * Cos(PI * u);
-
-		// Sphere with vertical bands
-		float r = 0.9f + 0.1f * Sin(8f * PI * u);
-		float s = r * Cos(0.5f * PI * v);
-		Vector3 p;
-		p.x = s * Sin(PI * u);
-		p.y = r * Sin(0.5f * PI * v);
-		p.z = s * Cos(PI * u);
-
-		// Shpere with horizontal bands
-		float r = 0.9f + 0.1f * Sin(8f * PI * v);
-		float s = r * Cos(0.5f * PI * v);
-		Vector3 p;
-		p.x = s * Sin(PI * u);
-		p.y = r * Sin(0.5f * PI * v);
-		p.z = s * Cos(PI * u);
-
-		*/
 
 		// Spiral Sphere
 		float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
@@ -120,29 +70,6 @@ public static class FunctionLibrary {
     */
 	public static Vector3 Torus (float u, float v, float t) {
 		Vector3 p;
-		/*
-		// Pulled apart shpere
-		float r = 1f;
-		float s = 0.5f + r * Cos(0.5f * PI * v);
-		p.x = s * Sin(PI * u);
-		p.y = r * Sin(0.5f * PI * v);
-		p.z = s * Cos(PI * u);
-
-		// A self-intersecting spindle torus.
-		float r = 1f;
-		float s = 0.5f + r * Cos(PI * v);
-		p.x = s * Sin(PI * u);
-		p.y = r * Sin(PI * v);
-		p.z = s * Cos(PI * u);
-
-		// A ring torus.
-		float r1 = 0.75f;
-		float r2 = 0.25f;
-		float s = r1 + r2 * Cos(PI * v);
-		p.x = s * Sin(PI * u);
-		p.y = r2 * Sin(PI * v);
-		p.z = s * Cos(PI * u);
-		*/
 
 		// Twisting torus.
 		float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
