4f18e2d80f77f5d0d656a5c7ab253e670da1471c
Culver Stone <c_stone157@hotmail.com>
2023-08-15 21:32:35 -0700

    Added the graph project again

---
+++ Assets/Scripts/2_2_Basics/02-BuildingAGraph/Graph.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+    // The prefab of our point object
+	[SerializeField]
+	Transform pointPrefab;
+
+    // Number of cubes to create
+	[SerializeField]
+	int resolution = 10;
+
+    // On the object awakening
+    void Awake () {
+        /*
+        // Instantiate: creates a copy of the object that is passed
+		// Instantiate(pointPrefab);
+        Transform point = Instantiate(pointPrefab);
+        // Shift our point to the right
+        point.localPosition = Vector3.right;
+
+        // Create a second point and shift even further to the right
+		point = Instantiate(pointPrefab);
+        point.localPosition = Vector3.right * 2f;
+        */
+
+        /*
+        // Loop to create all of our Points
+        //int i = 0;
+		//while (i < 10) {
+		//	i = i + 1;
+		for (int i = 0; i < 10; i++) {
+        	Transform point = Instantiate(pointPrefab);
+			point.localPosition = Vector3.right * i;
+            // Change the scale of our point
+            point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
+		}
+        */
+
+        var position = Vector3.zero;
+		var scale = Vector3.one / 5f;
+		for (int i = 0; i < 10; i++) {
+			Transform point = Instantiate(pointPrefab);
+			position.x = (i + 0.5f) / 5f - 1f;
+			//position.y = position.x;
+            position.y = position.x * position.x;
+			point.localPosition = position;
+			point.localScale = scale;
+		}
+    }
+}
