213a2376e0a4b7a9b34e68909f761fef78acc4e3
Culver Stone <c_stone157@hotmail.com>
2023-08-21 20:52:12 -0700

    cleaned up all of the old code

--- Assets/Scripts/2_2_Basics/02-BuildingAGraph/Graph.cs
+++ Assets/Scripts/2_2_Basics/02-BuildingAGraph/Graph.cs
@@ -7,62 +7,28 @@ public class Graph : MonoBehaviour {
 	Transform pointPrefab;
 
     // Number of cubes to create
-	//[SerializeField]
-    //[SerializeField, Range(10, 100)]
     [SerializeField, Range(10, 100)]
 	int resolution = 10;
 
-    //Transform points;
     Transform[] points;
 
     // On the object awakening
     void Awake () {
-        /*
-        // Instantiate: creates a copy of the object that is passed
-		// Instantiate(pointPrefab);
-        Transform point = Instantiate(pointPrefab);
-        // Shift our point to the right
-        point.localPosition = Vector3.right;
-
-        // Create a second point and shift even further to the right
-		point = Instantiate(pointPrefab);
-        point.localPosition = Vector3.right * 2f;
-        */
-
-        /*
-        // Loop to create all of our Points
-        //int i = 0;
-		//while (i < 10) {
-		//	i = i + 1;
-		for (int i = 0; i < 10; i++) {
-        	Transform point = Instantiate(pointPrefab);
-			point.localPosition = Vector3.right * i;
-            // Change the scale of our point
-            point.localPosition = Vector3.right * ((i + 0.5f) / 5f - 1f);
-		}
-        */
-
+        // Initial all of our variables
         float step = 2f / resolution;
         var position = Vector3.zero;
-		//var scale = Vector3.one / 5f;
 		var scale = Vector3.one * step;
-		//for (int i = 0; i < 10; i++) {
         
         points = new Transform[resolution];
 		for (int i = 0; i < points.Length; i++) {
-        	//Transform point = Instantiate(pointPrefab);
-            //points[i] = point;
+            // Create the transform for all of our points
 			Transform point = points[i] = Instantiate(pointPrefab);
 
-            //position.x = (i + 0.5f) / 5f - 1f;
+            // Assign the x position (since this never changes this is here, not in the update)
 			position.x = (i + 0.5f) * step - 1f;
-            //position.y = position.x;
-            //position.y = position.x * position.x;
-			//position.y = position.x * position.x * position.x;
             point.localPosition = position;
 			point.localScale = scale;
 
-            //point.SetParent(transform);
             // When a new parent is set Unity will attempt to keep 
             //  the object at its original world position, rotation, 
             //  and scale. We don't need this in our case. We can 
@@ -72,6 +38,7 @@ public class Graph : MonoBehaviour {
 		}
     }
 
+    // On the object updating
     void Update () {
         float time = Time.time;
 
@@ -79,12 +46,9 @@ public class Graph : MonoBehaviour {
 			Transform point = points[i];
 			Vector3 position = point.localPosition;
             
-            //position.y = position.x * position.x * position.x;
-            //position.y = Mathf.Sin(position.x);
-            //position.y = Mathf.Sin(Mathf.PI * position.x);
-            //position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
+            // Generate a sine wave, based upon time
             position.y = Mathf.Sin(Mathf.PI * (position.x + time));
-            
+
             point.localPosition = position;
 		}
 	}
