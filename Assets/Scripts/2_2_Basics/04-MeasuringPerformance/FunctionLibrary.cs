e93579917c5c740ec72d53bd6936e3c9e113ce74
Culver Stone <culver.stone57@gmail.com>
2023-10-02 20:57:06 -0700

    Started working on the measuring stats project

---
+++ Assets/Scripts/2_2_Basics/04-MeasuringPerformance/FunctionLibrary.cs
@@ -0,0 +1,85 @@
+using UnityEngine;
+using static UnityEngine.Mathf;
+
+namespace MeasuringPerformance
+{
+    public static class FunctionLibrary {
+        // Delegate is a special type that defines what kind of method something can reference
+        //public delegate float Function (float x, float z, float t);
+        public delegate Vector3 Function (float u, float v, float t);
+
+
+        // An enumeration of the function names
+        public enum FunctionName { Wave, MultiWave, Ripple, Sphere, RotatingSphere, Torus }
+        
+        // An array of functions rather than using numbers
+        static Function[] functions = { Wave, MultiWave, Ripple, Sphere, RotatingSphere, Torus };
+
+        // Get the function asked for
+        public static Function GetFunction (FunctionName name) {
+            return functions[(int)name];
+        }
+
+        // Define the Wave function for our library (using sine)
+        public static Vector3 Wave (float u, float v, float t) {
+            Vector3 p;
+            p.x = u;
+            p.y = Sin(PI * (u + v + t));
+            p.z = v;
+            return p;
+        }
+
+        // Define the function for our library, that is the multiwave
+        public static Vector3 MultiWave (float u, float v, float t) {
+            Vector3 p;
+            p.x = u;
+            p.y = Sin(PI * (u + 0.5f * t));
+            p.y += 0.5f * Sin(2f * PI * (v + t));
+            p.y += Sin(PI * (u + v + 0.25f * t));
+            p.y *= 1f / 2.5f;
+            p.z = v;
+            return p;
+        }
+
+            // Define a ripple funtion for our library
+        public static Vector3 Ripple (float u, float v, float t) {
+            float d = Sqrt(u * u + v * v);
+            Vector3 p;
+            p.x = u;
+            p.y = Sin(PI * (4f * d - t));
+            p.y /= 1f + 10f * d;
+            p.z = v;
+            return p;
+        }
+        
+        public static Vector3 Sphere (float u, float v, float t) {
+            // Plain Sphere
+            float r = Cos(0.5f * PI * v);
+            Vector3 p;
+            p.x = r * Sin(PI * u);
+            p.y = Sin(PI * 0.5f * v);
+            p.z = r * Cos(PI * u);
+            return p;
+        }
+        public static Vector3 RotatingSphere (float u, float v, float t) {
+            float r = 0.9f + 0.1f * Sin(PI * (6f * u + 4f * v + t));
+            float s = r * Cos(0.5f * PI * v);
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r * Sin(0.5f * PI * v);
+            p.z = s * Cos(PI * u);
+            return p;
+        }
+
+        public static Vector3 Torus (float u, float v, float t) {
+            float r1 = 0.7f + 0.1f * Sin(PI * (6f * u + 0.5f * t));
+            float r2 = 0.15f + 0.05f * Sin(PI * (8f * u + 4f * v + 2f * t));
+            float s = r1 + r2 * Cos(PI * v);
+            Vector3 p;
+            p.x = s * Sin(PI * u);
+            p.y = r2 * Sin(PI * v);
+            p.z = s * Cos(PI * u);
+            return p;
+        }
+    }
+}
---
+++ Assets/Scripts/2_2_Basics/04-MeasuringPerformance/Graph.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+using MathematicalSurfaces;
+
+/**
+ * NOTE: Skipped section 3.4 - Better Visuals.
+ *      I wasn't able to find the shadow setting section, doesn't matter
+ *      so I skipped it.
+**/
+namespace MeasuringPerformance
+{
+    public class Graph : MonoBehaviour {
+
+        // The prefab of our point object
+        [SerializeField]
+        Transform pointPrefab;
+
+        // Number of cubes to create
+        [SerializeField, Range(10, 100)]
+        int resolution = 10;
+
+        // Select a function to move the points by
+        [SerializeField]
+	    FunctionLibrary.FunctionName function;
+        
+        Transform[] points;
+
+        // On the object awakening
+        void Awake () {
+            // Initial all of our variables
+            float step = 2f / resolution;
+            var scale = Vector3.one * step;
+            
+            points = new Transform[resolution * resolution];
+            for (int i = 0; i < points.Length; i++) {
+                // Create the transform for all of our points
+                Transform point = points[i] = Instantiate(pointPrefab);
+
+                // Assign the x position (since this never changes this is here, not in the update)
+                point.localScale = scale;
+                point.SetParent(transform, false);
+
+                // When a new parent is set Unity will attempt to keep 
+                //  the object at its original world position, rotation, 
+                //  and scale. We don't need this in our case. We can 
+                //  signal this by passing false as a second argument 
+                //  to SetParent.
+                point.SetParent(transform, false);
+            }
+        }
+
+        // On the object updating
+        void Update () {
+            FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
+            float time = Time.time;
+            float step = 2f / resolution;
+            float v = 0.5f * step - 1f;
+            for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++) {
+                if (x == resolution) {
+                    x = 0;
+                    z += 1;
+                    v = (z + 0.5f) * step - 1f;
+                }
+                float u = (x + 0.5f) * step - 1f;
+                points[i].localPosition = f(u, v, time);
+            }
+        }
+
+    }
+}
